1280c4d376de23a4d14415fcf9b5a673587aa3a9
EDERANDER <ea.quispe@est.unap.edu.pe>
2025-09-28 20:11:22 -0500

    Insertando qr de yape

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 import logo from '../logo.svg'; // Using the existing logo for now
+import yapeImage from '../yape.jpeg';
 import { FaCalculator, FaChartBar, FaShareAlt } from 'react-icons/fa';
 
 function Inicio() {
@@ -23,28 +24,34 @@ function Inicio() {
   };
 
   return (
-    <div className="App inicio-page">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Bienvenido a la Calculadora de Puntaje</h1>
-        <p className="subtitle">Prepara tu postulación para la Beca de Permanencia</p>
-      </header>
+    <>
+      <div className="App inicio-page">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Bienvenido a la Calculadora de Puntaje</h1>
+          <p className="subtitle">Prepara tu postulación para la Beca de Permanencia</p>
+        </header>
 
-      <div className="inicio-actions">
-        <Link to="/formulario" className="btn btn-primary">
-          <FaCalculator className="btn-icon" />
-          Consulta tu Puntaje
-        </Link>
-        <a href="https://docs.google.com/spreadsheets/d/1XiYRQPXvstrfugqcWS5qJADG659ViIxq0aAzpKOiRTo/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="btn btn-secondary">
-          <FaChartBar className="btn-icon" />
-          Estadísticas Beca 2024
-        </a>
-        <button onClick={shareLink} className="btn btn-tertiary">
-          <FaShareAlt className="btn-icon" />
-          Compartir
-        </button>
+        <div className="inicio-actions">
+          <Link to="/formulario" className="btn btn-primary">
+            <FaCalculator className="btn-icon" />
+            Consulta tu Puntaje
+          </Link>
+          <a href="https://docs.google.com/spreadsheets/d/1XiYRQPXvstrfugqcWS5qJADG659ViIxq0aAzpKOiRTo/edit?usp=sharing" target="_blank" rel="noopener noreferrer" className="btn btn-secondary">
+            <FaChartBar className="btn-icon" />
+            Estadísticas Beca 2024
+          </a>
+          <button onClick={shareLink} className="btn btn-tertiary">
+            <FaShareAlt className="btn-icon" />
+            Compartir
+          </button>
+        </div>
       </div>
-    </div>
+      <div style={{ textAlign: 'center', marginTop: '20px', color: 'white' }}>
+        <img src={yapeImage} alt="Donación Yape" style={{ width: '200px' }} />
+        <p>Donación no obligatoria</p>
+      </div>
+    </>
   );
 }
 
